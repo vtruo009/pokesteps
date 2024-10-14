dde6b98c4c8750a9c43c900bfa5ed8ce546ef4fd
Alice Nguyen <alicenguyen@alices-mbp.lan>
2024-10-13 17:03:41 -0700

    ticket-2: added api structure basic; needs fine tuning on stricter types

---
+++ app/common/api/pokemon-calls.ts
@@ -0,0 +1,18 @@
+import { HttpStatusCode } from "axios"
+import { pokemonAPI } from "./api"
+import { ApiResponse } from "../interface/api.interface";
+
+interface getPokemonInfoResponse {
+    name: string;
+}
+
+export const getPokemonInfo = async (pokemonName: string): Promise<ApiResponse<getPokemonInfoResponse>> => {
+    const getPokemonURL = `pokemon/${pokemonName}`
+    const response = await pokemonAPI({method: 'GET', url: getPokemonURL})
+    if(response?.status === HttpStatusCode.Ok){
+        return response.data
+    } else {
+        //TODO: how to type response structure for error and valid responses
+        return {error: "something to indicate error here"}
+    }
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,30 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { useState } from "react";
+import { Text, SafeAreaView, Button } from "react-native";
+import { getPokemonInfo } from "./common/api/pokemon-calls";
 
 const Home = () => {
+	// Next few lines of code are just for reference. Can be removed once everyone gets more accustomed to using call structure.
+	const [pokemon, setPokemon] = useState("pokemon")
+
+	const testGetPokemon = async () => {
+		const dittoInfo = await getPokemonInfo('ditto')
+		if(dittoInfo.data){
+			setPokemon(dittoInfo.data.name)
+		}
+	}
+
+	const clearPokemon = () => {
+		setPokemon('')
+	}
+	// can delete this ^ once comfortable
+
 	return (
 		<SafeAreaView>
-			<Text>This is where you see all the pokemons</Text>
+			<Text>
+				pokemon is {pokemon}
+			</Text>
+			<Button title="test get ditto" onPress={testGetPokemon}/>
+			<Button title="clear pokemon" onPress={clearPokemon}/>
 		</SafeAreaView>
 	);
 };
