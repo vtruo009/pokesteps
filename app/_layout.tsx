c55d16a0a1530ed75e946ae4b9dc423634251e0a
Jonathan Huynh <jonathanhuynh@Jonathans-MacBook-Air-2.local>
2024-10-17 11:57:18 -0700

    added navigation bar and steps screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Stack, Tabs } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
@@ -27,8 +27,7 @@ export default function RootLayout() {
 
 	return (
 		<Stack>
-			<Stack.Screen name='index' options={{ headerShown: false }} />
-			<Stack.Screen name='pokedex' options={{ headerShown: false }} />
+			<Stack.Screen name='tabs' options={{ headerShown: false }} />
 			<Stack.Screen name='+not-found' />
 		</Stack>
 	);
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { Redirect } from "expo-router";
 
 const Home = () => {
-	return <Redirect href={"/pokedex"} />;
+	return <Redirect href={"/tabs/pokedex"} />;
 };
 
 export default Home;
