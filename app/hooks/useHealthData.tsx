a1b24e44c3960c9820c9e88fc58f4d6106bd4c13
Van Truong <van.t414@gmail.com>
2024-11-05 16:38:17 -0800

    create useHealthData hook

---
+++ app/hooks/useHealthData.tsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+import appleHealthKit, {
+	HealthInputOptions,
+	HealthKitPermissions,
+	HealthValue,
+} from "react-native-health";
+
+const useHealthData = () => {
+	const [permission, setPermission] = useState(false);
+	const [todaySteps, setTodaySteps] = useState(0);
+	const [yesterdaySteps, setYesterdaySteps] = useState(0);
+
+	const permissions: HealthKitPermissions = {
+		permissions: {
+			read: [appleHealthKit.Constants.Permissions.Steps],
+			write: [],
+		},
+	};
+
+	useEffect(() => {
+		appleHealthKit.initHealthKit(permissions, (error: string) => {
+			if (error) {
+				// TODO: Throw exception?
+				console.log(error);
+				return;
+			}
+			setPermission(true);
+		});
+	});
+
+	useEffect(() => {
+		if (!permission) return;
+
+		const today = new Date();
+		const yesterday = new Date(
+			today.getFullYear(),
+			today.getMonth(),
+			today.getDate() - 1
+		);
+		const options: HealthInputOptions = {
+			startDate: yesterday.toISOString(),
+			includeManuallyAdded: true,
+		};
+
+		appleHealthKit.getDailyStepCountSamples(
+			options,
+			(error: string, result: HealthValue[]) => {
+				if (error) {
+					// TODO: Throw exception?
+					console.log(error);
+					return;
+				}
+				console.log(result);
+				setTodaySteps(result[0].value);
+				setYesterdaySteps(result[1].value);
+			}
+		);
+	}, [permission]);
+
+	return {
+		todaySteps,
+		yesterdaySteps,
+	};
+};
+
+export default useHealthData;
