a7a1b2daea0db4c6f03198e6c8373487aa796afd
Van Truong <van.t414@gmail.com>
2024-11-09 20:42:51 -0800

    image & id stylized

--- app/root/pokemon-details.tsx
+++ app/root/pokemon-details.tsx
@@ -8,10 +8,12 @@ const PokemonDetails = () => {
 	const pokemon = useRoute().params as Pokemon;
 	return (
 		<SafeAreaView className='flex-1 w-full h-full justify-around bg-white'>
-			{/* <View className='flex-1 justify-center items-center'> */}
-			<PokemonCard pokemon={pokemon} width={60} height={60} />
-			<Text>{`#${pokemon.id?.toString().padStart(4, '0')}`}</Text>
-			{/* </View> */}
+			<View className='flex-1 justify-center items-center'>
+				<PokemonCard pokemon={pokemon} width={60} height={60} />
+				<Text className='text-xl text-gray-500 font-PixelifySans'>{`#${pokemon.id
+					?.toString()
+					.padStart(4, '0')}`}</Text>
+			</View>
 		</SafeAreaView>
 	);
 };
