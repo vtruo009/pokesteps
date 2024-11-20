61f08ab6614d261f01019c00db6a626d2f674c0c
Van Truong <van.t414@gmail.com>
2024-11-19 21:42:14 -0800

    utilize AsyncStorage

---
+++ app/utils/storageHelper.ts
@@ -0,0 +1,20 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const storeData = async (key: string, value: string) => {
+	try {
+		await AsyncStorage.setItem(key, JSON.stringify(value));
+	} catch (error) {
+		console.log('Error saving data', error);
+	}
+};
+
+export const getItemForKey = async (key: string) => {
+	try {
+		const value = await AsyncStorage.getItem(key);
+		if (value !== null) {
+			return JSON.parse(value);
+		}
+	} catch (error) {
+		console.log('Error getting data', error);
+	}
+};
