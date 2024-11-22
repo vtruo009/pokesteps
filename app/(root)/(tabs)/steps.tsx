5c44ceaf6984654e46e56b28c454a04bb9bc79fd
Van Truong <van.t414@gmail.com>
2024-11-22 14:59:47 -0800

    Steps Screen working

--- app/(root)/(tabs)/steps.tsx
+++ app/(root)/(tabs)/steps.tsx
@@ -2,12 +2,17 @@ import { Text, View } from 'react-native';
 import React from 'react';
 import useHealthData from '@/hooks/useHealthData';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import ProgressRing from '@/components/ProgressRing';
+
+const STEP_GOAL = 10000;
 
 export default function StepsHomeScreen() {
 	const { todaySteps, yesterdaySteps } = useHealthData();
+	const progress = todaySteps / STEP_GOAL;
 
 	return (
 		<SafeAreaView className='flex-1 justify-around items-center bg-white'>
+			<ProgressRing progress={progress} />
 			<View className='flex justify-center items-center font-PixelifySans'>
 				<Text className='text-6xl font-PixelifySans'>
 					{todaySteps.toLocaleString()}
