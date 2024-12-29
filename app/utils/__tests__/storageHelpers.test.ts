33a9cceaf90edc7e9c85160976a67dfb1ff86528
Van Truong <van.t414@gmail.com>
2024-12-28 20:54:56 -0800

    storageHelpers unit tests

---
+++ app/utils/__tests__/storageHelpers.test.ts
@@ -0,0 +1,10 @@
+import { StorageKeys } from '../storageHelpers';
+
+describe('Storage Helpers', () => {
+	it.todo('should get item from storage');
+	// it.each([
+	// 	[StorageKeys.HAS_LAUNCHED, 'true'],
+	// 	[StorageKeys.POKEMONS, '[]'],
+	// 	[StorageKeys.STEP_GOAL, '10000'],
+	// ])('should store item in storage', async (key, value) => {});
+});
