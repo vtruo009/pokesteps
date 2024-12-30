3e0fea76d4e698497eb017ba03d111bacf220d48
Van Truong <van.t414@gmail.com>
2024-12-29 19:56:06 -0800

    storageHelpers tests passing

--- __tests__/utils/storageHelpers.test.ts
+++ __tests__/utils/storageHelpers.test.ts
@@ -1,13 +1,33 @@
-import { getItemForKey, StorageKeys } from '@/app/utils/storageHelpers';
+import {
+	getItemForKey,
+	removeItemForKey,
+	StorageKeys,
+	storeData,
+} from '@/app/utils/storageHelpers';
+import AsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
 describe('Storage Helpers', () => {
-	it('should get item from storage', async () => {
+	beforeEach(async () => {
+		await removeItemForKey(StorageKeys.HAS_LAUNCHED);
+	});
+
+	it('should store item in storage', async () => {
+		expect(await getItemForKey(StorageKeys.HAS_LAUNCHED)).toBeUndefined();
+		await storeData(StorageKeys.HAS_LAUNCHED, 'true');
+		expect(await getItemForKey(StorageKeys.HAS_LAUNCHED)).toBe('true');
+	});
+
+	it('should get item from storage and return undefined', async () => {
 		const hasLaunched = await getItemForKey(StorageKeys.HAS_LAUNCHED);
 		expect(hasLaunched).toBeUndefined();
+		expect(AsyncStorage.getItem).toHaveBeenCalledWith(StorageKeys.HAS_LAUNCHED);
 	});
-	// it.each([
-	// 	[StorageKeys.HAS_LAUNCHED, 'true'],
-	// 	[StorageKeys.POKEMONS, '[]'],
-	// 	[StorageKeys.STEP_GOAL, '10000'],
-	// ])('should store item in storage', async (key, value) => {});
+
+	it('should get item from storage and return a value', async () => {
+		const expectedHasLaunched = 'true';
+		await storeData(StorageKeys.HAS_LAUNCHED, expectedHasLaunched);
+		const hasLaunched = await getItemForKey(StorageKeys.HAS_LAUNCHED);
+		expect(hasLaunched).toBeDefined();
+		expect(hasLaunched).toBe(expectedHasLaunched);
+	});
 });
