8e4dca71dbd49d2af7fba16636a6710e2dcfe102
Van Truong <van.t414@gmail.com>
2025-01-04 11:27:29 -0800

    follow 3-part test format

--- __tests__/utils/storageHelpers.test.ts
+++ __tests__/utils/storageHelpers.test.ts
@@ -13,20 +13,26 @@ describe('Storage Helpers', () => {
 
 	it('should store item in storage', async () => {
 		expect(await getItemForKey(StorageKeys.HAS_LAUNCHED)).toBeUndefined();
+
 		await storeData(StorageKeys.HAS_LAUNCHED, 'true');
+
 		expect(await getItemForKey(StorageKeys.HAS_LAUNCHED)).toBe('true');
 	});
 
 	it('should get nonexistent item from storage and return undefined', async () => {
 		const hasLaunched = await getItemForKey(StorageKeys.HAS_LAUNCHED);
+
 		expect(hasLaunched).toBeUndefined();
+
 		expect(AsyncStorage.getItem).toHaveBeenCalledWith(StorageKeys.HAS_LAUNCHED);
 	});
 
 	it('should get existing item from storage and return stored value', async () => {
 		const expectedHasLaunched = 'true';
+
 		await storeData(StorageKeys.HAS_LAUNCHED, expectedHasLaunched);
 		const hasLaunched = await getItemForKey(StorageKeys.HAS_LAUNCHED);
+
 		expect(hasLaunched).toBeDefined();
 		expect(hasLaunched).toBe(expectedHasLaunched);
 	});
