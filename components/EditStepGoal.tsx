555b2e2d0e161b33a47e00d2b6bb542e1dc4d58c
Van Truong <van.t414@gmail.com>
2025-01-08 18:48:09 -0800

    fix steps.tsx

--- components/EditStepGoal.tsx
+++ components/EditStepGoal.tsx
@@ -65,7 +65,7 @@ const EditStepGoal = ({
 				<TextInput
 					placeholder='Enter new step goal'
 					keyboardType='number-pad'
-					className='border border-gray-300 rounded-md px-3 py-2 m-1  font-JetBrainsMonoExtraLight'
+					className='border border-gray-300 rounded-md px-3 py-2 m-1 font-JetBrainsMonoExtraLight'
 					autoFocus
 					clearButtonMode='while-editing'
 					style={{ width: wp('50%'), height: 'auto' }}
