49009975c3bad96b6a15d8c3138b2c27a8e7c140
Van Truong <43657607+vtruo009@users.noreply.github.com>
2025-02-08 17:10:12 -0800

    convert to js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import usersRoutes from './routes/users';
-import pokemonsRoutes from './routes/pokemons';
+import usersRoutes from './routes/users.js';
+import pokemonsRoutes from './routes/pokemons.js';
 
 const app = express();
 const PORT = process.env.PORT || 4242;
@@ -12,6 +12,12 @@ app.use(express.json());
 app.use('/users', usersRoutes);
 app.use('/pokemons', pokemonsRoutes);
 
+app.get('/', (req, res) => {
+	res.send('Hello!');
+});
+
 app.listen(PORT, () => {
 	console.log(`Listening to http://localhost:${PORT}`);
 });
+
+export default app;
